f186a59036251310c2679b4f3d67c989a9cc5cbc
PedroAvila <minimainor_austin@hotmail.com>
2021-08-07 22:43:30 -0500

    Configuración de Firebase

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: false,
-  name: 'dev'
+  name: 'dev',
+  firebase: {
+    Config: {
+      apiKey: "AIzaSyDKIW0j38UpYamGFAQHQqci7TmAH1-d4Bw",
+      authDomain: "ecommerce-c0e61.firebaseapp.com",
+      projectId: "ecommerce-c0e61",
+      storageBucket: "ecommerce-c0e61.appspot.com",
+      messagingSenderId: "774282341166",
+      appId: "1:774282341166:web:1e8a1327487a014b5528bf"
+    }
+  }
 };
